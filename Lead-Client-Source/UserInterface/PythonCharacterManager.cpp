13da0f3c864918b1b71604dc0aba0dd3596841fc
Lead0x194 <not_found@lead.com>
2026-03-08 06:33:44 +0100

    fix: TraceError message

--- Lead-Client-Source/UserInterface/PythonCharacterManager.cpp
+++ Lead-Client-Source/UserInterface/PythonCharacterManager.cpp
@@ -469,7 +469,7 @@ CInstanceBase * CPythonCharacterManager::CreateInstance(const CInstanceBase::SCr
 	CInstanceBase * pCharacterInstance = RegisterInstance(c_rkCreateData.m_dwVID);
 	if (!pCharacterInstance)
 	{
-		TraceError("CPythonCharacterManager::CreateInstance: VID[%d] - ALREADY EXIST\n", c_rkCreateData);
+		TraceError("CPythonCharacterManager::CreateInstance: VID[%d] - ALREADY EXIST\n", c_rkCreateData.m_dwVID);
 		return NULL;
 	}
 
